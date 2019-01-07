faa8e2cd3f63f82c334c29f9108290a754d9e000
robertogamez <roberto.mb.gamez@hotmail.com>
2019-01-06 21:26:30 -0600

    Ejemplo de Factory Pattern

---
+++ FactoryPattern/Core/ChartProviderFree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Core
+{
+    public class ChartProviderFree : IChartProvider
+    {
+        public IChart GetChart()
+        {
+            IChart chart = new BarChart();
+            return chart;
+        }
+    }
+}
---
+++ FactoryPattern/Core/ChartProviderPaid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Core
+{
+    public class ChartProviderPaid : IChartProvider
+    {
+        public IChart GetChart()
+        {
+            IChart chart = new PieChart();
+            return chart;
+        }
+    }
+}
